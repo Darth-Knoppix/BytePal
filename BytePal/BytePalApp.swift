f57cab81ee41e781826d83e39bef311465d265b9
Seth Corker <seth.corker@gmail.com>
2021-05-24 15:40:58 +0100

    Initial Commit

---
+++ BytePal/BytePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BytePalApp.swift
+//  BytePal
+//
+//  Created by Seth Corker on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BytePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
